fde8ad5599b9f3852a2a5e0b8d93619f87055c28
navrang9 <nagy.akos.andras@gmail.com>
2024-11-23 19:09:14 +0100

    Adding draft mdx query

---
+++ airflow_provider_tm1/operators/tm1_mdx_query.py
@@ -0,0 +1,172 @@
+from typing import Any, Callable, Collection, Mapping, Sequence
+
+import pandas as pd
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator
+from airflow.utils.context import Context
+from airflow.utils.decorators import apply_defaults
+from airflow.utils.operator_helpers import KeywordParameters
+
+from airflow_provider_tm1.hooks.tm1 import TM1Hook
+
+
+class MDXQueryOperator(BaseOperator):
+    """
+    This operator executes an MDX query
+
+    :param mdx: Valid MDX Query
+    :param top: Int, number of cells to return (counting from top)
+    :param skip: Int, number of cells to skip (counting from top)
+    :param skip_zeros: skip zeros in cellset (irrespective of zero suppression in MDX / view)
+    :param skip_consolidated_cells: skip consolidated cells in cellset
+    :param skip_rule_derived_cells: skip rule derived cells in cellset
+    :param sandbox_name: str
+    :param include_attributes: include attribute columns
+    :param use_iterative_json: use iterative json parsing to reduce memory consumption significantly.
+    Comes at a cost of 3-5% performance.
+    :param use_compact_json: bool
+    :param use_blob: Has better performance on datasets > 1M cells and lower memory footprint in any case.
+    :param shaped: preserve shape of view/mdx in data frame
+    :param mdx_headers: boolean, fully qualified hierarchy name as header instead of simple dimension name
+    :param fillna_numeric_attributes: boolean, fills empty numerical attributes with fillna_numeric_attributes_value
+    :param fillna_string_attributes: boolean, fills empty string attributes with fillna_string_attributes_value
+    :param fillna_numeric_attributes_value: Any, value with which to replace na if fillna_numeric_attributes is True
+    :param fillna_string_attributes_value: Any, value with which to replace na if fillna_string_attributes is True
+    :param tm1_conn_id: The Airflow connection used for TM1 credentials.
+    :param tm1_dry_run: in Dry mode the Operator will skip the execution. Default value is False.
+    :param post_hook: A reference to a callable, which is executed taking after the MDX completed using its result DataFrame as input.
+    :param op_kwargs: a dictionary of keyword arguments that will get unpacked
+        in your function
+    :param op_args: a list of positional arguments that will get unpacked when
+        calling your callable
+    :param templates_dict: a dictionary where the values are templates that
+        will get templated by the Airflow engine sometime between
+        ``__init__`` and ``execute`` takes place and are made available
+        in your callable's context after the template has been applied. (templated)
+    :param templates_exts: a list of file extensions to resolve while
+        processing templated fields, for examples ``['.sql', '.hql']``
+    :param show_return_value_in_logs: a bool value whether to show return_value
+        logs. Defaults to True, which allows return value log output.
+        It can be set to False to prevent log output of return value when you return huge data
+        such as transmission a large amount of XCom to TaskAPI.
+    """
+
+    @apply_defaults
+    def __init__(
+            self,
+            mdx: str,
+            top: int = None,
+            skip: int = None,
+            skip_zeros: bool = True,
+            skip_consolidated_cells: bool = False,
+            skip_rule_derived_cells: bool = False,
+            sandbox_name: str = None,
+            include_attributes: bool = False,
+            use_iterative_json: bool = False,
+            use_compact_json: bool = False,
+            use_blob: bool = False,
+            shaped: bool = False,
+            mdx_headers: bool = False,
+            fillna_numeric_attributes: bool = False,
+            fillna_numeric_attributes_value: Any = 0,
+            fillna_string_attributes: bool = False,
+            fillna_string_attributes_value: Any = '',
+            tm1_conn_id: str = "tm1_default",
+            tm1_dry_run: bool = False,
+            post_hook: Callable = None,
+            op_args: Collection[Any] | None = None,
+            op_kwargs: Mapping[str, Any] | None = None,
+            templates_dict: dict[str, Any] | None = None,
+            templates_exts: Sequence[str] | None = None,
+            show_return_value_in_logs: bool = True,
+            *args,
+            **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.mdx = mdx
+        if not callable(post_hook):
+            raise AirflowException("`post_hook` param must be callable")
+        self.top = top
+        self.skip = skip
+        self.skip_zeros = skip_zeros
+        self.skip_consolidated_cells = skip_consolidated_cells
+        self.skip_rule_derived_cells = skip_rule_derived_cells
+        self.sandbox_name = sandbox_name
+        self.include_attributes = include_attributes
+        self.use_iterative_json = use_iterative_json
+        self.use_compact_json = use_compact_json
+        self.use_blob = use_blob
+        self.shaped = shaped
+        self.mdx_headers = mdx_headers
+        self.fillna_numeric_attributes = fillna_numeric_attributes
+        self.fillna_numeric_attributes_value = fillna_numeric_attributes_value
+        self.fillna_string_attributes = fillna_string_attributes
+        self.fillna_string_attributes_value = fillna_string_attributes_value
+        self.tm1_conn_id = tm1_conn_id
+        self.tm1_dry_run = tm1_dry_run
+        self.post_hook = post_hook
+        self.op_args = op_args or ()
+        self.op_kwargs = op_kwargs or {}
+        self.templates_dict = templates_dict
+        if templates_exts:
+            self.template_ext = templates_exts
+        self.show_return_value_in_logs = show_return_value_in_logs
+
+    def execute(self, context: dict) -> None:
+        tm1_hook = TM1Hook(tm1_conn_id=self.tm1_conn_id)
+        tm1 = tm1_hook.get_conn()
+
+        if not self.tm1_dry_run:
+
+            df = tm1.cells.execute_mdx_dataframe(mdx=self.mdx, top=self.top, skip=self.skip, skip_zeros=self.skip_zeros,
+                                                 skip_consolidated_cells=self.skip_consolidated_cells,
+                                                 skip_rule_derived_cells=self.skip_rule_derived_cells,
+                                                 sandbox_name=self.sandbox_name,
+                                                 include_attributes=self.include_attributes,
+                                                 use_iterative_json=self.use_iterative_json,
+                                                 use_compact_json=self.use_compact_json,
+                                                 use_blob=self.use_blob, shaped=self.shaped,
+                                                 mdx_headers=self.mdx_headers,
+                                                 fillna_numeric_attributes=self.fillna_numeric_attributes,
+                                                 fillna_numeric_attributes_value=self.fillna_numeric_attributes_value,
+                                                 fillna_string_attributes=self.fillna_string_attributes,
+                                                 fillna_string_attributes_value=self.fillna_string_attributes_value)
+
+            context_merge(context, self.op_kwargs, templates_dict=self.templates_dict)
+            self.op_kwargs = self.determine_kwargs(context)
+
+            return_value = self.execute_callable(df)
+            if self.show_return_value_in_logs:
+                self.log.info("Done. Returned value was: %s", return_value)
+            else:
+                self.log.info("Done. Returned value not shown")
+            return return_value
+
+        else:
+            print("Execution TM1 MDX " + self.mdx + " in dry-run mode")
+
+    def determine_kwargs(self, context: Mapping[str, Any]) -> Mapping[str, Any]:
+        return KeywordParameters.determine(self.post_hook, self.op_args, context).unpacking()
+
+    def execute_callable(self, df: pd.DataFrame) -> Any:
+        """
+        Call the python callable with the given arguments.
+
+        :return: the return value of the call.
+        """
+        return self.post_hook(df, *self.op_args, **self.op_kwargs)
+
+
+def context_merge(context: Context, *args: Any, **kwargs: Any) -> None:
+    """Merge parameters into an existing context.
+
+    Like ``dict.update()`` , this take the same parameters, and updates
+    ``context`` in-place.
+
+    This is implemented as a free function because the ``Context`` type is
+    "faked" as a ``TypedDict`` in ``context.pyi``, which cannot have custom
+    functions.
+
+    :meta private:
+    """
+    context.update(*args, **kwargs)
---
+++ tests_integration/dags/airflow_test_exeucte_mdx.py
@@ -0,0 +1,48 @@
+from datetime import timedelta
+
+import pandas as pd
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from airflow_provider_tm1.operators.tm1_mdx_query import MDXQueryOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+
+def parse_and_filter(df: pd.DataFrame):
+    print("test1 dim values:" + str(df.test1.values))
+    print("test2 dim values:" + str(df.test2.values))
+
+
+with DAG(
+        'airflow_test_success_dag',
+        default_args=default_args,
+        schedule_interval=None,
+        start_date=days_ago(1),
+        tags=[],
+        catchup=False,
+        max_active_runs=1
+) as dag:
+    t1 = MDXQueryOperator(
+        task_id='t1',
+        tm1_conn_id='tm1_conn',
+        mdx="""
+           SELECT 
+           {[test2].[test2].Members} 
+           ON COLUMNS , 
+           {[test1].[test1].Members} 
+           ON ROWS 
+           FROM [test1] 
+           """,
+        post_hook=parse_and_filter
+    )
+
+    t1
