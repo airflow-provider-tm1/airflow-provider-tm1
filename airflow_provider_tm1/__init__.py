5750eb58cf675ec38b98b4340e0dbe89c3526c57
navrang9 <nagy.akos.andras@gmail.com>
2024-11-27 13:43:21 +0100

    Adding release and pypi push support

--- airflow_provider_tm1/__init__.py
+++ airflow_provider_tm1/__init__.py
@@ -1,6 +1,22 @@
 """A TM1 provider package for Airflow"""
 __version__ = "0.0.10"
 
+import os
+import re
+
+def update_version():
+    version_file = os.path.join(os.path.dirname(__file__), '__init__.py')
+    with open(version_file, 'r') as f:
+        content = f.read()
+    new_version = os.environ.get('VERSION')
+    content_new = re.sub(r'__version__ = ["\'].*["\']', f'__version__ = "{new_version}"', content, 1)
+    with open(version_file, 'w') as f:
+        f.write(content_new)
+
+
+def get_version():
+    return __version__
+
 
 def get_provider_info():
     return {
@@ -13,5 +29,5 @@ def get_provider_info():
                 "hook-class-name": "airflow_provider_tm1.hooks.tm1.TM1Hook",
             },
         ],
-        "version": [__version__],
+        "version": [get_version()],
     }
