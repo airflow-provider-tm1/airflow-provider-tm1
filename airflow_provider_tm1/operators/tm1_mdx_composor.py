ba26418c9e4bed3b90d4221b4c5e12b7b471483e
Joe.Chow <jchow@cubewise.com>
2024-12-06 09:57:32 +0800

    init: project plan

---
+++ airflow_provider_tm1/operators/tm1_mdx_composor.py
@@ -0,0 +1,12 @@
+def compose():
+    """
+    consume the dictionary, , 
+    """
+
+    #todo: build the template 
+    #todo: parse it to subset mdx for each 
+    #todo:join them into cube query mdx 
+
+    #? need to validate cube? 
+    #! be careful on the ] or any other special character 
+    
